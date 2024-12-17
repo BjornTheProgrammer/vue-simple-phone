81aa480113078e4bc3a0c03ced6167508ee5313c
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-12-17 02:39:07 -0700

    Add base for favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	title: 'Vue Simple Phone',
 	description: 'A simple vue component for phone number inputs',
 	base: '/vue-simple-phone/',
-	head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+	head: [['link', { rel: 'icon', href: '/vue-simple-phone/favicon.ico' }]],
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		nav: [
