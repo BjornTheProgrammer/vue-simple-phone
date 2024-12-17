e34efcc06fdc668bff3ebd35d8d9617901dd8508
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-12-16 21:28:19 -0700

    Add base for github pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
 	title: 'Vue Simple Phone',
 	description: 'A simple vue component for phone number inputs',
+	base: '/vue-simple-phone/',
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		nav: [
