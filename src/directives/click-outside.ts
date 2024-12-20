3c469aebb3e4d9bd87b432372183ad0081687f5a
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-12-20 14:24:39 -0700

    chore: fix lints

--- src/directives/click-outside.ts
+++ src/directives/click-outside.ts
@@ -2,7 +2,8 @@ import type { Directive } from 'vue';
 
 export const vClickOutside: Directive = {
 	beforeMount: (el, binding) => {
-		if (typeof binding.value !== 'function') throw new Error('Binding must be a function');
+		if (typeof binding.value !== 'function')
+			throw new Error('Binding must be a function');
 		el.clickOutsideEvent = (event: MouseEvent) => {
 			if (!(el === event.target || el.contains(event.target))) binding.value();
 		};
