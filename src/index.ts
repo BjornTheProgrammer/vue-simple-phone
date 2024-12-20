e60558611aebf1b7e18d9eb026855c96549b532e
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-12-20 13:34:17 -0700

    feat: using dialog instead of div

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import VueSimplePhone from './VueSimplePhone.vue';
+import VueSimplePhone from './components/VueSimplePhone.vue';
 
 export { countries as supportedCountries } from 'country-flag-icons';
 export { VueSimplePhone };
